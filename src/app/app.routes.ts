81f16db2c9591dbf2d9329cdf456a9f74710aa32
Abbas Ashourloo <abbas.ashourlou@gmail.com>
2025-11-02 14:42:01 +0330

    Add Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './+pages/home-page/home-page';
+import { AboutPage } from './+pages/about-page/about-page';
+import { ProductsPage } from './+pages/products-page/products-page';
+import { SupportPage } from './+pages/support-page/support-page';
+import { ContactPage } from './+pages/contact-page/contact-page';
+import { NotFoundPage } from './+pages/not-found-page/not-found-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomePage },
+  { path: 'about', component: AboutPage },
+  { path: 'products', component: ProductsPage },
+  { path: 'support', component: SupportPage },
+  { path: 'contact', component: ContactPage },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component:NotFoundPage },
+];
